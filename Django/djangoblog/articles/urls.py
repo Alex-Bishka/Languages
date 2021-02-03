3a33a9587b9f85993051d20870262a8a87b26e37
Alex-Bishka <abishka@g.hmc.edu>
2021-02-02 21:47:04 -0600

    links for slugs have been added into the templates and url files

--- Django/djangoblog/articles/urls.py
+++ Django/djangoblog/articles/urls.py
@@ -7,8 +7,11 @@ We still need to import views and the urlpatterns.
 re_path() will allow the use of regular expressions (path() does not). 
 """
 
+# name spacing our url file
+app_name = 'articles'
+
 urlpatterns = [
-    path('', views.article_list),
+    path('', views.article_list, name="list"),
     # this will extend the url to the name - or rather slug - of the article
-    re_path(r'^(?P<slug>[\w-]+)/$', views.article_detail),
+    re_path(r'^(?P<slug>[\w-]+)/$', views.article_detail, name="detail"),
 ]
