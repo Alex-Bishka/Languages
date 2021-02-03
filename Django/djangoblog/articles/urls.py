c26a8fba7ac0fe310be43e4f8112f2414ddf226c
Alex-Bishka <abishka@g.hmc.edu>
2021-02-02 21:33:48 -0600

    slugs now work for articles!

--- Django/djangoblog/articles/urls.py
+++ Django/djangoblog/articles/urls.py
@@ -1,10 +1,14 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 """
 We still need to import views and the urlpatterns.
+
+re_path() will allow the use of regular expressions (path() does not). 
 """
 
 urlpatterns = [
     path('', views.article_list),
+    # this will extend the url to the name - or rather slug - of the article
+    re_path(r'^(?P<slug>[\w-]+)/$', views.article_detail),
 ]
--- Django/djangoblog/articles/views.py
+++ Django/djangoblog/articles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Article
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -18,8 +19,16 @@ in the templates folder in the root dir
 
 def article_list(request):
     """
+    responsible for rendering our list of articles, specifically
+    the template defined by the second arguement and the data
+    (in the third arg) that feeds into the template
     """
     # the data we will send to the template
     articles = Article.objects.all().order_by('date')
 
     return render(request, 'articles/article_list.html', {'articles': articles}) # here is where namespacing is nice
+
+def article_detail(request, slug):
+    """
+    """
+    return HttpResponse(slug)
