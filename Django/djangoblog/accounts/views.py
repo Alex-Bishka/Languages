f23e0d83b85b28b46aa67a8d5e36b655cd3d5e1d
Alex-Bishka <abishka@g.hmc.edu>
2021-02-07 17:15:30 -0600

    initialized the default sign up form and passed it into the render function

--- Django/djangoblog/accounts/views.py
+++ Django/djangoblog/accounts/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def signup_view(request):
-    return render(request, 'accounts/signup.html')
+    form = UserCreationForm()  # creates a new instance of the form
+
+    # the third parameter is a dictionary of data we can pass in
+    # this data is passed into the html template listed in our second
+    # parameter
+    return render(request, 'accounts/signup.html', {'form':form})  
