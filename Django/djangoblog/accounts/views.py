59986380c0961346f7419e772f71d6885e9edd8c
Alex-Bishka <abishka@g.hmc.edu>
2021-02-08 12:49:02 -0600

    added in logic to login the user

--- Django/djangoblog/accounts/views.py
+++ Django/djangoblog/accounts/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 # Create your views here.
 def signup_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()  # this will save the data from a valid form
-            # next step is to log in the user
+            user = form.save()  # this will save the data from a valid form and gives us the user
+            # log in the user
+            login(request, user)
+
+            # redirect the user after logging them in
             return redirect('articles:list')
     else:
         # get request case
@@ -24,7 +28,11 @@ def login_view(request):
         # b/c it is not naturally the first param in the method
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-            # next step is to log in the user
+            # log in the user
+            user = form.get_user()
+            login(request, user)
+
+            # send user to the articles page
             return redirect('articles:list')
     else:
         # get request
