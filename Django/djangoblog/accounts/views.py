4eaca97dc9b3974954987468ca8ee0fc6f281b08
Alex-Bishka <abishka@g.hmc.edu>
2021-02-08 12:14:45 -0600

    signup_view now handles both post and get requests for the sign up form

--- Django/djangoblog/accounts/views.py
+++ Django/djangoblog/accounts/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def signup_view(request):
-    form = UserCreationForm()  # creates a new instance of the form
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()  # this will save the data from a valid form
+            # next step is to log in the user
+            return redirect('articles:list')
+    else:
+        # get request case
+        form = UserCreationForm()  # creates a new instance of the form
 
     # the third parameter is a dictionary of data we can pass in
     # this data is passed into the html template listed in our second
