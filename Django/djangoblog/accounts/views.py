d892b934afa7ea10a8b9e3155575a8d64b8965ad
Alex-Bishka <abishka@g.hmc.edu>
2021-02-08 12:40:38 -0600

    added in a view for login requests

--- Django/djangoblog/accounts/views.py
+++ Django/djangoblog/accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 def signup_view(request):
@@ -17,3 +17,16 @@ def signup_view(request):
     # this data is passed into the html template listed in our second
     # parameter
     return render(request, 'accounts/signup.html', {'form':form})  
+
+def login_view(request):
+    if request.method == "POST":
+        # we have to specify the data parameter in this case
+        # b/c it is not naturally the first param in the method
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            # next step is to log in the user
+            return redirect('articles:list')
+    else:
+        # get request
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form':form})
