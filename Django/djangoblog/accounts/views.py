d4fb288b3683e5c1153833a61aa1456e41fa7b91
Alex-Bishka <abishka@g.hmc.edu>
2021-02-08 19:17:02 -0600

    added logout method

--- Django/djangoblog/accounts/views.py
+++ Django/djangoblog/accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 
 # Create your views here.
 def signup_view(request):
@@ -38,3 +38,10 @@ def login_view(request):
         # get request
         form = AuthenticationForm()
     return render(request, 'accounts/login.html', {'form':form})
+
+def logout_view(request):
+    if request.method == "POST":
+        # don't need to pass in the current user, as we are already
+        # logged in, and Django knows this
+        logout(request)
+        return redirect('articles:list')
