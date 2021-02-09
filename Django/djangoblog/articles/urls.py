df775f8faf794a9f5a74c58e7eef95a400c4c53e
Alex-Bishka <abishka@g.hmc.edu>
2021-02-08 19:40:12 -0600

    added create path

--- Django/djangoblog/articles/urls.py
+++ Django/djangoblog/articles/urls.py
@@ -10,8 +10,10 @@ re_path() will allow the use of regular expressions (path() does not).
 # name spacing our url file
 app_name = 'articles'
 
+# create must come above the slug, or else django will think that create is a slug
+# re_path will extend the url to the name - or rather slug - of the article
 urlpatterns = [
     path('', views.article_list, name="list"),
-    # this will extend the url to the name - or rather slug - of the article
+    path('create/', views.article_create, name='create'),
     re_path(r'^(?P<slug>[\w-]+)/$', views.article_detail, name="detail"),
 ]
